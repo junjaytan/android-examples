1995417f45068085f27149a0472d61466a755ae8
Junjay Tan <junjay@junjaytan.com>
2015-12-07 23:55:32 -0500

    working GeoQuiz Ch 1 app

--- app/src/main/java/com/example/junjaytan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/junjaytan/geoquiz/QuizActivity.java
@@ -8,24 +8,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT).show();
             }
         });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
