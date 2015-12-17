8d01a48d89c023dcabd90f2c65ebfed7f568bebe
Junjay Tan <junjay@junjaytan.com>
2015-12-17 00:34:43 -0500

    fixed bug-broken next button

---
+++ app/src/main/java/com/example/junjaytan/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.example.junjaytan.geoquiz;
+
+/**
+ * Created by junjaytan on 12/9/15.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
