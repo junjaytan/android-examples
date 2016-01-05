b518f542896e51ce280ef3e5c3362920dba1493f
Junjay Tan <junjay@junjaytan.com>
2016-01-05 01:29:02 -0500

    finished Ch 13

--- CriminalIntent/app/src/main/java/criminalintent/android/junjaytan/com/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/criminalintent/android/junjaytan/com/criminalintent/CrimePagerActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,7 +16,7 @@ import java.util.UUID;
 /**
  * Created by junjaytan on 1/3/16.
  */
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
     private static final String EXTRA_CRIME_ID =
             "criminalintent.android.junjaytan.com.criminalintent.crime_id";
 
