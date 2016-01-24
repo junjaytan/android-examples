d68cadcadd97d6baf5939c89bbab71151a5a646e
Junjay Tan <junjay@junjaytan.com>
2016-01-23 20:39:35 -0500

    Finished Ch 18: Assets

---
+++ BeatBox/app/src/main/java/com/junjaytan/beatbox/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.junjaytan.beatbox;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by junjaytan on 1/2/16.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager manager = getFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
