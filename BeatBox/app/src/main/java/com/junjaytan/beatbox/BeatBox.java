0b4ebf6dbfc1091a0185352916f4834a51b31259
Junjay Tan <junjay@junjaytan.com>
2016-01-25 23:23:29 -0500

    Finished Ch 19: Audio Playback with SoundPool

--- BeatBox/app/src/main/java/com/junjaytan/beatbox/BeatBox.java
+++ BeatBox/app/src/main/java/com/junjaytan/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.junjaytan.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -15,13 +18,30 @@ public class BeatBox {
     private static final String TAG = "BeatBox";
 
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        // This old constructor is deprecated, but we need it for compatibility
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
+    }
+
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0 ,1.0f);
+    }
+
+    // after releasing below, you clean up the sound in BeatBoxFragment
+    public void release() {
+        mSoundPool.release();
     }
 
     private void loadSounds() {
@@ -37,11 +57,23 @@ public class BeatBox {
         }
 
         for (String filename : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Could not load sound " + filename, ioe);
+            }
 
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+
         }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
