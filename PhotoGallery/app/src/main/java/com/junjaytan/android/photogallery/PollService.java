a8ca264c8114d2940de4730c6fef728e7f5599fa
Junjay Tan <junjay.tan@tamr.com>
2016-02-22 01:49:26 -0500

    finished up to Limiting Broadcasts to your app

--- PhotoGallery/app/src/main/java/com/junjaytan/android/photogallery/PollService.java
+++ PhotoGallery/app/src/main/java/com/junjaytan/android/photogallery/PollService.java
@@ -22,6 +22,9 @@ public class PollService extends IntentService {
     private static final String TAG = "PollService";
     private static final long POLL_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
 
+    public static final String ACTION_SHOW_NOTIFICATION =
+            "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION";
+
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
@@ -95,6 +98,7 @@ public class PollService extends IntentService {
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(0, notification);
+        sendBroadcast(new Intent(ACTION_SHOW_NOTIFICATION));
 
         QueryPreferences.setLastResultId(this, resultId);
     }
