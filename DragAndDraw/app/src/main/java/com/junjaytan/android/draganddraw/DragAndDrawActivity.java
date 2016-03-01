2595ed80f4f7f9d7a6c51c1154b7e271a937dc51
Junjay Tan <junjay@junjaytan.com>
2016-03-01 01:39:18 -0500

    Finished Ch 29: Custom Views and Touch Events

---
+++ DragAndDraw/app/src/main/java/com/junjaytan/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,12 @@
+package com.junjaytan.android.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
+    }
+
+}
