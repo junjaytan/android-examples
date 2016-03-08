52c57c494c316775f3ca01f8b7b835c0b7b9a72f
Junjay Tan <junjay@junjaytan.com>
2016-03-07 23:59:51 -0500

    Finished Ch 31: Locations and Play Services

---
+++ Locatr/app/src/main/java/com/junjaytan/android/locatr/GalleryItem.java
@@ -0,0 +1,59 @@
+package com.junjaytan.android.locatr;
+
+import android.net.Uri;
+
+/**
+ * Created by junjaytan on 2/11/16.
+ */
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    private String mOwner;
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
