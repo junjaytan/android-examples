252640f86c755022b1ea833ee2d59b3b03a0c842
Junjay Tan <junjay@junjaytan.com>
2016-03-08 00:46:17 -0500

    Finished Ch 32: Maps

--- Locatr/app/src/main/java/com/junjaytan/android/locatr/GalleryItem.java
+++ Locatr/app/src/main/java/com/junjaytan/android/locatr/GalleryItem.java
@@ -9,6 +9,8 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private double mLat;
+    private double mLon;
 
     private String mOwner;
 
@@ -26,6 +28,22 @@ public class GalleryItem {
 
     public void setmId(String mId) {
         this.mId = mId;
+    }
+
+    public double getLat() {
+        return mLat;
+    }
+
+    public void setLat(double lat) {
+        mLat = lat;
+    }
+
+    public double getLon() {
+        return mLon;
+    }
+
+    public void setLon(double lon) {
+        mLon = lon;
     }
 
     public String getmUrl() {
